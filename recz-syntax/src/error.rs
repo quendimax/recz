82b2d9b5b0b006d76f65fe14a9006b2f2e21c281
Maksim Tomkavich <quendimax@gmail.com>
2026-08-27 13:34:54 +0200

    refactor(syntax): make `miette` dep optional

--- recz-syntax/src/error.rs
+++ recz-syntax/src/error.rs
@@ -1,4 +1,3 @@
-use miette::{Diagnostic, SourceSpan};
 use recz_codec as codec;
 use recz_graph::CaptureLabel;
 use std::ops::Range;
@@ -6,15 +5,16 @@ use thiserror::Error;
 
 pub type Result<T> = std::result::Result<T, Box<Error>>;
 
-#[derive(Error, Diagnostic, Debug, PartialEq)]
+#[derive(Error, Debug, PartialEq)]
+#[cfg_attr(feature = "miette", derive(miette::Diagnostic))]
 pub enum Error {
     #[error("codec error: {cause}")]
     CodecError {
         #[source]
         cause: codec::Error,
 
-        #[label("invalid codepoint")]
-        span: SourceSpan,
+        #[cfg_attr(feature = "miette", label("invalid codepoint"))]
+        span: Range<usize>,
     },
 
     #[error("expected {expected}, but found `{misspell}`")]
@@ -22,8 +22,8 @@ pub enum Error {
         misspell: Box<str>,
         expected: Box<str>,
 
-        #[label("unexpected token")]
-        span: SourceSpan,
+        #[cfg_attr(feature = "miette", label("unexpected token"))]
+        span: Range<usize>,
     },
 
     #[error("value `{value}` is out of {range}")]
@@ -31,75 +31,75 @@ pub enum Error {
         value: Box<str>,
         range: Box<str>,
 
-        #[label("invalid value")]
-        span: SourceSpan,
+        #[cfg_attr(feature = "miette", label("invalid value"))]
+        span: Range<usize>,
     },
 
     #[error("empty escape expression is not allowed")]
     EmptyEscape {
-        #[label("empty escape")]
-        span: SourceSpan,
+        #[cfg_attr(feature = "miette", label("empty escape"))]
+        span: Range<usize>,
     },
 
     #[error("unsupported escape sequence `{sequence}`")]
     UnsupportedEscape {
         sequence: Box<str>,
 
-        #[label("unsupported escape")]
-        span: SourceSpan,
+        #[cfg_attr(feature = "miette", label("unsupported escape"))]
+        span: Range<usize>,
     },
 
     #[error("zero repetition `{{0,0}}` is not allowed")]
     ZeroRepetition {
-        #[label("zero repetition")]
-        span: SourceSpan,
+        #[cfg_attr(feature = "miette", label("zero repetition"))]
+        span: Range<usize>,
     },
 
     #[error("repetition expression `{{n,m}}` expects that `n <= m`")]
     InvalidRepetition {
-        #[label("invalid repetition")]
-        span: SourceSpan,
+        #[cfg_attr(feature = "miette", label("invalid repetition"))]
+        span: Range<usize>,
     },
 
     #[error("using capture label `{label}` more than once is not allowed")]
     CaptureLabelReuse {
         label: CaptureLabel,
 
-        #[label("group name is already used")]
-        span: SourceSpan,
+        #[cfg_attr(feature = "miette", label("group name is already used"))]
+        span: Range<usize>,
     },
 
     #[error(
-        r"range `[{range}]` is inverted: first codepoint `\x{fcp:X}` is greater than last one `\x{lcp:X}`"
+        r"range `{range}` is inverted: first codepoint `\x{fcp:X}` is greater than last one `\x{lcp:X}`"
     )]
-    #[diagnostic(help("try swapping the codepoints"))]
+    #[cfg_attr(feature = "miette", diagnostic(help("try swapping the codepoints")))]
     InvertedRange {
         range: Box<str>,
         fcp: u32,
         lcp: u32,
 
-        #[label("inverted range")]
-        span: SourceSpan,
+        #[cfg_attr(feature = "miette", label("inverted range"))]
+        span: Range<usize>,
     },
 
     #[error(r"capture label contains disallowed characters")]
     InvalidCaptureLabelChar {
-        #[label("disallowed character")]
-        span: SourceSpan,
+        #[cfg_attr(feature = "miette", label("disallowed character"))]
+        span: Range<usize>,
     },
 
     #[error(r"group with prefix `{prefix}` is not supported")]
     UnsupportedGroup {
         prefix: Box<str>,
 
-        #[label("the prefix")]
-        span: SourceSpan,
+        #[cfg_attr(feature = "miette", label("the prefix"))]
+        span: Range<usize>,
     },
 
     #[error(r"capture group label must contain at least one character")]
     EmptyCaptureLabel {
-        #[label("empty capture label")]
-        span: SourceSpan,
+        #[cfg_attr(feature = "miette", label("empty capture label"))]
+        span: Range<usize>,
     },
 }
 
@@ -108,10 +108,7 @@ pub(crate) mod err {
     use super::*;
 
     pub(crate) fn codec_error<T>(cause: codec::Error, span: Range<usize>) -> Result<T> {
-        Err(Box::new(Error::CodecError {
-            cause,
-            span: span.into(),
-        }))
+        Err(Box::new(Error::CodecError { cause, span }))
     }
 
     pub(crate) fn unexpected<T>(
@@ -121,7 +118,7 @@ pub(crate) mod err {
     ) -> Result<T> {
         Err(Box::new(Error::Unexpected {
             misspell: misspell.into(),
-            span: misspan.into(),
+            span: misspan,
             expected: expected.into(),
         }))
     }
@@ -133,13 +130,13 @@ pub(crate) mod err {
     ) -> Result<T> {
         Err(Box::new(Error::OutOfRange {
             value: value.into(),
-            span: span.into(),
+            span,
             range: range.into(),
         }))
     }
 
     pub(crate) fn empty_escape<T>(span: Range<usize>) -> Result<T> {
-        Err(Box::new(Error::EmptyEscape { span: span.into() }))
+        Err(Box::new(Error::EmptyEscape { span }))
     }
 
     pub(crate) fn unsupported_escape<T, S>(sequence: S, span: Range<usize>) -> Result<T>
@@ -148,16 +145,16 @@ pub(crate) mod err {
     {
         Err(Box::new(Error::UnsupportedEscape {
             sequence: sequence.into(),
-            span: span.into(),
+            span,
         }))
     }
 
     pub(crate) fn zero_repetition<T>(span: Range<usize>) -> Result<T> {
-        Err(Box::new(Error::ZeroRepetition { span: span.into() }))
+        Err(Box::new(Error::ZeroRepetition { span }))
     }
 
     pub(crate) fn invalid_repetition<T>(span: Range<usize>) -> Result<T> {
-        Err(Box::new(Error::InvalidRepetition { span: span.into() }))
+        Err(Box::new(Error::InvalidRepetition { span }))
     }
 
     pub(crate) fn reuse_capture_label<T>(
@@ -166,7 +163,7 @@ pub(crate) mod err {
     ) -> Result<T> {
         Err(Box::new(Error::CaptureLabelReuse {
             label: label.into(),
-            span: span.into(),
+            span,
         }))
     }
 
@@ -180,13 +177,13 @@ pub(crate) mod err {
             range: range.into(),
             fcp: first_codepoint,
             lcp: last_codepoint,
-            span: span.into(),
+            span,
         }))
     }
 
     pub(crate) fn invalid_capture_label_char<T>(disallows_char_span: Range<usize>) -> Result<T> {
         Err(Box::new(Error::InvalidCaptureLabelChar {
-            span: disallows_char_span.into(),
+            span: disallows_char_span,
         }))
     }
 
@@ -196,11 +193,11 @@ pub(crate) mod err {
     ) -> Result<T> {
         Err(Box::new(Error::UnsupportedGroup {
             prefix: prefix.into(),
-            span: span.into(),
+            span,
         }))
     }
 
     pub(crate) fn empty_capture_label<T>(span: Range<usize>) -> Result<T> {
-        Err(Box::new(Error::EmptyCaptureLabel { span: span.into() }))
+        Err(Box::new(Error::EmptyCaptureLabel { span }))
     }
 }
