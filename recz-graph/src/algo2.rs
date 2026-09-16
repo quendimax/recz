d7c21cb09fc1424b97b51cb47ff8d08c3ebe5f46
Maksim Tomkavich <quendimax@gmail.com>
2026-09-17 00:09:30 +0200

    perf(graph): optimize algo2

--- recz-graph/src/algo2.rs
+++ recz-graph/src/algo2.rs
@@ -1,5 +1,6 @@
 use crate::{Graph, Node, Tag};
 use recz_adt::{Map, OrdSet, Set};
+use std::cell::Cell;
 use std::hash::{DefaultHasher, Hash, Hasher};
 use std::rc::Rc;
 
@@ -14,8 +15,7 @@ pub fn determine(nfa: &Graph) -> Graph {
 struct Determinator<'d, 'n> {
     nfa: &'n Graph,
     dfa: &'d Graph,
-    conv_table: Map<Rc<OrdSet<Node<'n>>>, Node<'d>>,
-    closure_eval: EClosureEval<'n>,
+    closure_eval: EClosureEval<'d, 'n>,
     final_tags: Map<Node<'d>, Vec<Tag>>,
 }
 
@@ -30,7 +30,6 @@ impl<'d, 'n> Determinator<'d, 'n> {
         Self {
             nfa,
             dfa,
-            conv_table: Map::default(),
             closure_eval: EClosureEval::new(),
             final_tags: Map::default(),
         }
@@ -63,9 +62,13 @@ impl<'d, 'n> Determinator<'d, 'n> {
         }
     }
 
-    fn recurse(&mut self, closure: Rc<EClosure<'n>>) -> Node<'d> {
+    fn recurse(&mut self, closure: Rc<EClosure<'d, 'n>>) -> Node<'d> {
+        if let Some(dfa_node) = closure.dfa_node.get() {
+            return dfa_node;
+        }
+
         let dfa_node = self.dfa.node();
-        self.conv_table.insert(Rc::clone(&closure.nodes), dfa_node);
+        closure.dfa_node.set(Some(dfa_node));
 
         if let Some(clo_final_tags) = &closure.final_tags {
             dfa_node.finalize();
@@ -74,14 +77,11 @@ impl<'d, 'n> Determinator<'d, 'n> {
             self.final_tags.insert(dfa_node, final_tags);
         }
 
-        dbg!(dfa_node);
-
         for (symbol, (tags, nodes)) in &closure.sym_table {
             let sym_closure = self.closure_eval.eval(Rc::clone(nodes));
-            let sym_dfa_node = self
-                .conv_table
-                .get(&sym_closure.nodes)
-                .copied()
+            let sym_dfa_node = sym_closure
+                .dfa_node
+                .get()
                 .unwrap_or_else(|| self.recurse(sym_closure));
             let edge = dfa_node.connect(sym_dfa_node);
             edge.add_symbol(*symbol);
@@ -93,7 +93,7 @@ impl<'d, 'n> Determinator<'d, 'n> {
 }
 
 #[derive(Debug)]
-struct EClosure<'n> {
+struct EClosure<'d, 'n> {
     /// Epsilon closure of the nodes.
     nodes: Rc<OrdSet<Node<'n>>>,
 
@@ -105,27 +105,32 @@ struct EClosure<'n> {
     /// All tags that are going to the final nodes. If it is `None` the closure
     /// doesn't have any final nodes.
     final_tags: Option<Set<Tag>>,
+
+    /// The DFA node corresponding to this closure, filled in lazily once the
+    /// closure has been visited by the determinizer.
+    dfa_node: Cell<Option<Node<'d>>>,
 }
 
-impl<'n> EClosure<'n> {
+impl<'d, 'n> EClosure<'d, 'n> {
     pub fn default() -> Rc<Self> {
         Rc::new(Self {
             nodes: Rc::new(OrdSet::default()),
             sym_table: Map::default(),
             final_tags: None,
+            dfa_node: Cell::new(None),
         })
     }
 }
 
 #[derive(Debug)]
-struct EClosureEval<'n> {
-    cache: Map<Rc<OrdSet<Node<'n>>>, Rc<EClosure<'n>>>,
+struct EClosureEval<'d, 'n> {
+    cache: Map<Rc<OrdSet<Node<'n>>>, Rc<EClosure<'d, 'n>>>,
     visited: Map<Node<'n>, Set<u64>>,
     tag_collector: TagCollector,
-    closure: Rc<EClosure<'n>>,
+    closure: Rc<EClosure<'d, 'n>>,
 }
 
-impl<'n> EClosureEval<'n> {
+impl<'d, 'n> EClosureEval<'d, 'n> {
     fn new() -> Self {
         Self {
             cache: Map::default(),
@@ -135,7 +140,7 @@ impl<'n> EClosureEval<'n> {
         }
     }
 
-    fn eval(&mut self, start_nodes: Rc<OrdSet<Node<'n>>>) -> Rc<EClosure<'n>> {
+    fn eval(&mut self, start_nodes: Rc<OrdSet<Node<'n>>>) -> Rc<EClosure<'d, 'n>> {
         if let Some(closure) = self.cache.get(&start_nodes) {
             return Rc::clone(closure);
         }
@@ -152,7 +157,6 @@ impl<'n> EClosureEval<'n> {
         self.cache.insert(start_nodes, Rc::clone(&self.closure));
         let mut return_closure = EClosure::default();
         std::mem::swap(&mut self.closure, &mut return_closure);
-        // dbg!(&return_closure);
         return_closure
     }
 
@@ -167,11 +171,9 @@ impl<'n> EClosureEval<'n> {
 
         if node.is_final() {
             let closure = Rc::get_mut(&mut self.closure).unwrap();
-            dbg!("final");
             if let Some(tags) = &mut closure.final_tags {
                 merge_tags(tags, &self.tag_collector);
             } else {
-                dbg!(&self.tag_collector.tags);
                 closure.final_tags = Some(Set::from_iter(self.tag_collector.tags()));
             }
         }
@@ -184,12 +186,10 @@ impl<'n> EClosureEval<'n> {
             } else {
                 let closure = Rc::get_mut(&mut self.closure).unwrap();
                 for symbol in edge.symbols() {
-                    dbg!(symbol);
                     if let Some((tags, sym_closure)) = closure.sym_table.get_mut(&symbol) {
                         merge_tags(tags, &self.tag_collector);
                         sym_closure.insert(target);
                     } else {
-                        dbg!(&self.tag_collector.tags);
                         closure.sym_table.insert(
                             symbol,
                             (
@@ -279,8 +279,6 @@ impl TagCollector {
 
 /// The Grail of this determinization algorithm. It decides how tags are merged.
 fn merge_tags(dest_tags: &mut Set<Tag>, source_tags: &TagCollector) {
-    dbg!(&source_tags.tags);
-
     if dest_tags.is_empty() {
         return;
     }
